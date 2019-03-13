fda96fbc22b7e0418264a11af8fe30ce3907daac
Xavier Garceau-Aranda <xavier.garceau-aranda@nccgroup.com>
2019-03-13 14:07:55 +0100

    Restore previous date calculation

--- ScoutSuite/providers/azure/facade/monitor.py
+++ ScoutSuite/providers/azure/facade/monitor.py
@@ -9,10 +9,15 @@ class MonitorFacade:
         self._client = MonitorManagementClient(credentials, subscription_id)
 
     async def get_activity_logs(self):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        utc_now = datetime.datetime.utcnow()
+        end_time = utc_now.strftime(time_format)
+        timespan = datetime.timedelta(90)  # 90 days of timespan
+        start_time = (utc_now - timespan).strftime(time_format)
 
         list_filter = " and ".join([
-            "eventTimestamp le {}".format(datetime.datetime.now()),  # before today
-            "eventTimestamp ge {}".format(datetime.datetime.now() - datetime.timedelta(days=90)),  # after 90 days ago
+            "eventTimestamp ge {}".format(start_time),  # before today
+            "eventTimestamp le {}".format(end_time),  # after 90 days ago
             # The below filter makes the query *significantly* faster
             # Additional resources can be fetched according to need
             "resourceProvider eq {}".format('Microsoft.Storage/storageAccounts')
